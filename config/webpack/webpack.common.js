d65cb1c138d87421d3e35c8cb5149025c684fcaf
Daria Moskalets <daria1992moskalets@gmail.com>
2023-05-12 14:34:02 +0300

    fixed build rule for webpack f

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -141,7 +141,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.scss$/,
         exclude: /node_modules/,
         use: [
           // Creates `style` nodes from JS strings
