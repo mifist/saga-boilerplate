b44ea9020249dcc25e585b3bbeeb347c76782467
Daria Moskalets <daria1992moskalets@gmail.com>
2023-05-15 19:16:06 +0300

    fixed errors with not correct data

--- src/legacy/pages/Login/saga.js
+++ src/legacy/pages/Login/saga.js
@@ -1,67 +1,12 @@
 import { put, takeLatest } from 'redux-saga/effects';
-import { push } from 'connected-react-router';
 import requestWrapper from 'utils/requestWrapper';
-import { Capacitor } from '@capacitor/core';
 import i18n from 'i18next';
 import moment from 'moment';
+import { push } from 'redux-first-history';
+import { Capacitor } from '@capacitor/core';
 
 import * as CONSTANS from './constants';
 import * as ACTIONS from './actions';
-
-export function* loadUser(action) {
-  try {
-    // we retrieve the token from the local storage
-    // const token = localStorage.getItem('token');
-    // we get the campaigns linked to the user
-
-    let isProduction = 'development';
-    if (process.env.NODE_ENV === 'production') {
-      if (window.location.origin.includes('app.beemed.com')) {
-        isProduction = 'production';
-      } else {
-        isProduction = 'staging';
-      }
-    }
-
-    if (Capacitor.platform !== 'web') {
-      isProduction = 'mobile';
-    }
-
-    const user = yield requestWrapper(
-      'POST',
-      `auth/beemed`,
-      {
-        code: action.code,
-        env: isProduction,
-      },
-      '',
-    );
-    // console.debug(user);
-    // console.debug('GET THE USER SUCCESS');
-    if (user?._id) {
-      localStorage.setItem(
-        'beemed_user',
-        JSON.stringify({
-          access_token: user.access_token,
-          _id: user._id,
-          token: user.token,
-          role: user.role,
-        }),
-      );
-
-      moment.locale(user.language);
-      i18n.changeLanguage(user.language);
-      localStorage.setItem('cometchat:locale', user.language);
-
-      yield put(ACTIONS.loadUserSuccess(user));
-    } else {
-      console.warn('token expire', user);
-      yield put(ACTIONS.loadUserError(user));
-    }
-  } catch (err) {
-    yield put(ACTIONS.loadUserError(err));
-  }
-}
 
 export function* login(action) {
   try {
@@ -88,7 +33,7 @@ export function* login(action) {
       localStorage.setItem('cometchat:locale', response.user.language);
 
       yield put(ACTIONS.loginSuccess(response.user));
-      yield put(ACTIONS.push('/'));
+      yield put(push('/'));
     } else {
       console.warn('token expire', response);
       yield put(ACTIONS.loginError(response));
